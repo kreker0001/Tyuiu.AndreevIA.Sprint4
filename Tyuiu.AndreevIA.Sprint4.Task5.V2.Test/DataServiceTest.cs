65191507b6fe8b5d58b6616e8726bc9b22bddf0c
kreker0001 <krona154@gmail.com>
2025-11-13 17:59:37 +0500

    Task5

---
+++ Tyuiu.AndreevIA.Sprint4.Task5.V2.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.AndreevIA.Sprint4.Task5.V2.Lib;
+
+namespace Tyuiu.AndreevIA.Sprint4.Task5.V2.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = new int[5, 5] { { 0, -2, -4, 5, -4 },
+                                            { 4, -4, 5, -6, 7 },
+                                            { -7, -5, 6, 6, -6 },
+                                            { -6, -7, 6, 6, -4 },
+                                            { -5, -6, -6, -6, -5 } };
+
+            int wait = 7;
+
+            var rez = ds.Calculate(matrix);
+
+            Assert.AreEqual(wait, rez);
+        }
+    }
+}
---
+++ Tyuiu.AndreevIA.Sprint4.Task5.V2/Program.cs
@@ -0,0 +1,48 @@
+﻿using Tyuiu.AndreevIA.Sprint4.Task5.V2.Lib;
+
+Random rnd = new Random();
+DataService ds = new DataService();
+Console.Title = "Спринт #4 | Выполнил Андреев И. А. | РППб-25-1";
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* Спринт #4                                                               *");
+Console.WriteLine("* Тема: Двумерные массивы. (генератор случайных чисел)                    *");
+Console.WriteLine("* Задание #5                                                              *");
+Console.WriteLine("* Вариант #4                                                              *");
+Console.WriteLine("* Выполнил: Андреев Иван Александрович | РППб-25-1                        *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* УСЛОВИЕ:                                                                *");
+Console.WriteLine("* Дан двумерный целочисленный массив 5 на 5 элементов, заполненный        *");
+Console.WriteLine("* случайными значениями в диапазоне от -7 до 5. Найти количество          *");
+Console.WriteLine("* отрицательных элементов.                                                *");
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+Console.WriteLine("***************************************************************************");
+
+int[,] matrix = new int[5, 5];
+
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        matrix[i, j] = rnd.Next(-4, 6);
+    }
+}
+
+Console.WriteLine("\nИсходный массив:");
+for (int i = 0; i < 5; i++)
+{
+    for (int j = 0; j < 5; j++)
+    {
+        Console.Write($"{matrix[i, j]} \t");
+    }
+    Console.WriteLine();
+}
+Console.WriteLine();
+
+Console.WriteLine("***************************************************************************");
+Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+Console.WriteLine("***************************************************************************");
+var result = ds.Calculate(matrix);
+Console.WriteLine("Количество положительных элементов = " + result);
+Console.ReadKey();
